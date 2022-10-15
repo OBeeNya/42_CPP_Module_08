5f1b47cbf2f4bc680852a1a59be4efb5a92950a8
Benjamin AUBIGNAT <baubigna@e3r7p1.clusters.42paris.fr>
2022-10-15 17:34:21 +0200

    ex02

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,56 +2,56 @@
 
 int	main(void) {
 
+	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+
 	// {
-	// 	Span sp = Span(5);
-	// 	sp.addNumber(6);
-	// 	sp.addNumber(3);
-	// 	sp.addNumber(17);
-	// 	sp.addNumber(9);
-	// 	sp.addNumber(11);
-	// 	std::cout << sp.shortestSpan() << std::endl;
-	// 	std::cout << sp.longestSpan() << std::endl;
+	// 	Span	span(5);
+
+	// 	std::cout << std::endl;
+	// 	std::cout << " *** Trying to find the shortest span with no element ***" << std::endl;
+	// 	try { std::cout << span.shortestSpan() << std::endl; }
+	// 	catch (std::exception &e) { std::cerr << e.what() << std::endl; }
+
+	// 	std::cout << std::endl;
+	// 	std::cout << " *** Trying to add more elements than possible ***" << std::endl;
+	// 	for (int i = 0; i < 5; i++) span.addNumber(i);
+	// 	try { span.addNumber(5); }
+	// 	catch (std::exception &e) { std::cerr << e.what() << std::endl; }
+
+	// 	std::cout << std::endl;
+	// 	std::cout << " *** Finding the shortest spam ***" << std::endl;
+	// 	try { std::cout << span.shortestSpan() << std::endl; }
+	// 	catch (std::exception &e) { std::cerr << e.what() << std::endl; }
+
+	// 	std::cout << std::endl;
+	// 	std::cout << " *** Finding the longest spam ***" << std::endl;
+	// 	try { std::cout << span.longestSpan() << std::endl; }
+	// 	catch (std::exception &e) { std::cerr << e.what() << std::endl; }
 	// }
 
-	{
-		Span	span(5);
+	// {
+	// 	Span				span(100);
+	// 	std::vector<int>	range = {1, 2, 3, 4, 5, 6};
 
-		std::cout << std::endl;
-		std::cout << " *** Trying to find the shortest span with no element ***" << std::endl;
-		try { std::cout << span.shortestSpan() << std::endl; }
-		catch (std::exception &e) { std::cerr << e.what() << std::endl; }
-
-		std::cout << std::endl;
-		std::cout << " *** Trying to add more elements than possible ***" << std::endl;
-		for (int i = 0; i < 5; i++) span.addNumber(i);
-		try { span.addNumber(5); }
-		catch (std::exception &e) { std::cerr << e.what() << std::endl; }
-
-		std::cout << std::endl;
-		std::cout << " *** Finding the shortest spam ***" << std::endl;
-		try { std::cout << span.shortestSpan() << std::endl; }
-		catch (std::exception &e) { std::cerr << e.what() << std::endl; }
-
-		std::cout << std::endl;
-		std::cout << " *** Finding the longest spam ***" << std::endl;
-		try { std::cout << span.longestSpan() << std::endl; }
-		catch (std::exception &e) { std::cerr << e.what() << std::endl; }
-	}
-
-	{
-		Span				span(100);
-		std::vector<int>	range = {1, 2, 3, 4, 5, 6};
-
-		std::cout << std::endl;
-		std::cout << " *** Adding range until span is full ***" << std::endl;
-		int i = 20;
-		while (i--) {
-			try { span.addRange(range.begin(), range.end());
-				std::cout << i << ": range added..." << std::endl;
-			}
-			catch (std::exception &e) { std::cout << e.what() << std::endl; }
-		}
-	}
+	// 	std::cout << std::endl;
+	// 	std::cout << " *** Adding range until span is full ***" << std::endl;
+	// 	int i = 20;
+	// 	while (i--) {
+	// 		try { span.addRange(range.begin(), range.end());
+	// 			std::cout << i << ": range added..." << std::endl;
+	// 		}
+	// 		catch (std::exception &e) { std::cout << e.what() << std::endl; }
+	// 	}
+	// }
 
 	std::cout << std::endl;
 	return (0);
---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <list>
+# include <iostream>
+
+template <typename T>
+class MutantStack:
+	public std::stack<T>
+{
+	public:
+
+		typedef typename std::stack<T>::container_type::iterator	iterator;
+
+		MutantStack<T>() {}
+		MutantStack<T> (const MutantStack &src) { *this = src; }
+		~MutantStack<T> () {}
+
+		MutantStack	&operator=(const MutantStack &rhs) {
+			if (this == &rhs) return (*this);
+			this->c = rhs.c;
+			return (*this);
+		}
+
+		iterator	begin() { return (this->c.begin()); }
+		iterator	end() { return (this->c.end()); }
+};
+
+#endif
